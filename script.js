224c1291cd397d2bd3beb817210b95c1862c71eb
boxsports <bayramxdsa@gmail.com>
2025-06-02 19:47:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 const channels = [
-  { name: "BEIN SPORTS 1", url: "https://example.com/bein1.m3u8" },
+  { name: "BEIN SPORTS 1", url: "https://lll.istekbet3.tv/yayinzirve.m3u8" },
   { name: "BEIN SPORTS 2", url: "https://example.com/bein2.m3u8" },
   { name: "BEIN SPORTS 3", url: "https://example.com/bein3.m3u8" },
   { name: "S SPORT", url: "https://example.com/ssport.m3u8" },
