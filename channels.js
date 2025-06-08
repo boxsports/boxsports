e9835ab944a603918067710babee3e51382ac377
boxsports <bayramxdsa@gmail.com>
2025-06-08 15:44:37 +0300

    Create channels.js

---
+++ channels.js
@@ -0,0 +1,10 @@
+const channels = [
+  { name: "BEIN SPORTS 1", url: "https://bs.arectv33.sbs/static/bs1.m3u8" },
+  { name: "BEIN SPORTS 2", url: "https://bs.arectv33.sbs/static/bs2.m3u8" },
+  { name: "BEIN SPORTS 3", url: "https://bs.arectv33.sbs/static/bs3.m3u8" },
+  { name: "BEIN SPORTS 4", url: "https://bs.arectv33.sbs/static/bs4.m3u8" },
+  { name: "BEIN SPORTS 5", url: "https://bs.arectv33.sbs/static/bs5.m3u8" },
+  { name: "BEIN SPORTS MAX 1", url: "https://tv.arectv34.sbs/live/bm1.m3u8" },
+  { name: "BEIN SPORTS MAX 2", url: "https://tv.arectv34.sbs/live/bm2.m3u8" },
+  { name: "S SPORT", url: "https://example.com/ssport.m3u8" }
+];
